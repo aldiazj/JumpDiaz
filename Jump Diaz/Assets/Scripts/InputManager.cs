311aff1ea6323e706b24787c5b1f256e5be09aad
aldiazj <alejandrodiazjllo@gmail.com>
2018-07-13 23:00:36 -0500

    Adds and defines the Player and InputManager Classes
    Also creates the "Floor" tag

---
+++ Jump Diaz/Assets/Scripts/InputManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance = null;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<InputManager>();
+
+                if (instance == null)
+                {
+                    GameObject go = new GameObject(typeof(InputManager).ToString());
+                    instance = go.AddComponent<InputManager>();
+
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    Player player;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        // Player verification and Input handling
+        if (player)
+        {
+            // Left or A Input
+            if (Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A)) { player.MovePlayerHorizontally(Vector2.left); }
+            // Rigth or D Input
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) { player.MovePlayerHorizontally(Vector2.right); }
+            // Up or Space Input
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space)) { }
+        }
+	}
+
+    public void AssignPlayer(Player p)
+    {
+        player = p;
+    }
+}
